2a39eb5d0467de8623a1d2a02ce7d3505558cfa9
Nefton <neftonjin@yahoo.com>
2023-02-08 17:06:21 +0000

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # weather-api
+
+# Mitrea Mihai Radu 
+
+
+### Details about the application
+
+- This application is showing the weather forecast on the current day and on the next five days
+- You need to type the desired city and the weather will be displayed 
+- The weather application takes in your city name convert them into longitude and latitude and the shows the weather on that exact location 
+
+### Using the app
+- In order to use the application you need to type a city and then press search
+- After you type and search a city a history will appear on the left side of the screen
+- If you need to check again the same city you don`t have to type it again you just have to click on it 
+
+### Known bugs  ( to be fixed)
+- when pressing the history button the city name is not changing accordingly only the forecast details
+- history buttons not implemented in localstorage yet so if you refresh history will disappear
+- if no city is typed or an incorrect city name is passed to the search an error will appear in console 
+
+### Links 
+
+   Repository   https://github.com/neftonjin/weather-api
+
+   Github Page   https://neftonjin.github.io/weather-api/
+
+
+### App Preview
+
+
+<img width="1354" alt="Uasdfsfsdfsdfntitled" src="https://user-images.githubusercontent.com/25286630/217599499-29be8243-7241-45d7-b135-d3b366e183eb.png">
--- assets/logic.js
+++ assets/logic.js
@@ -10,9 +10,9 @@ $(document).ready(function () {
         const inputValue = $("#search-input").val().trim();
         const geoLocationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + inputValue + "&appid=" + apiKey + "&limit=1";
         let locationName = "";
-        let brokenUrl=fetch(geoLocationUrl);
+        
 
-    if (inputValue === "" || undefined || null || brokenUrl===false){ alert("Please enter a valid city name !!")}  else{
+    // if (inputValue === "" || undefined || null || brokenUrl===false){ alert("Please enter a valid city name !!")}  else{
 
     
      //This function is making the requests
@@ -21,7 +21,8 @@ $(document).ready(function () {
                 .then(firstResponse => firstResponse.json())
                 .then(firstData => {
                     locationName = firstData[0].name;
-                    console.log("This is the first data name" + firstData[0].name);
+                     console.log("This is the first data name :" + firstData[0].name);
+                     localStorage.setItem("name" ,firstData[0].name);
                     return fetch("https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + firstData[0].lat + "&lon=" + firstData[0].lon + "&cnt=6&appid=" + apiKey + "&units=metric")
                         .then(secondResponse => secondResponse.json())
                         .then(secondData => {
@@ -45,14 +46,14 @@ $(document).ready(function () {
             $("#subTitle").text(title);
             $("#forecast").empty();
             $("#today").empty();
-            console.log("The name " + data.city.name);
+            // console.log("The name " + data.city.name);
           
            //Function for displaying the forecast on the html
 
             function displayForecast(data) {
                 for (let i = 0; i < data.list.length; i++) {
                     if (i === 0) {
-
+                           console.log("this is the location name :" +locationName);
                         date = moment.unix(data.list[i].dt).format("DD/MM/YYYY");
                         let li = "<ul id='current_date' class='day'> <li id='city_name' > " + locationName + " </li>   <li id='date' class='forecast date'> " + date + " </li>   <li id='icon' class='forecast '> <img class='icon' src='http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png' " +
                             " </li>  <li id='humidity' class='forecast'> Humidity : " + data.list[i].humidity +
@@ -63,7 +64,7 @@ $(document).ready(function () {
 
                     } else {
                         date = moment.unix(data.list[i].dt).format("DD/MM/YYYY");
-
+                        //   console.log("this is not need to go onlly 5 times  ");
                         let li = "<ul class='day'> <li class='forecast date'> " + date + " </li>   <li  class='forecast '> <img class='icon' src='http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png' " +
                             " </li>  <li class='forecast'> Humidity : " + data.list[i].humidity +
                             "</li>     <li  class='forecast temp'> Temp: " + data.list[i].temp.day +
@@ -83,7 +84,7 @@ $(document).ready(function () {
 
             displayForecast(data);
             renderButtons();   
-
+            
 
             //This click event is dealing with the history buttons
             //Every time a button is clicked the ajax call is doing the same thing as the fetch api above is getting to requests one for geolocation and another one for the forecast
@@ -94,8 +95,8 @@ $(document).ready(function () {
                 let buttonClick = event.target.getAttribute('data-name');
                 console.log(" this is the history input " + buttonClick);
                 const geoLocationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + buttonClick + "&appid=" + apiKey + "&limit=1";
-
-
+               
+                
                 $.ajax({
                     method: "GET",
                     url: geoLocationUrl,
@@ -106,7 +107,7 @@ $(document).ready(function () {
                         // console.log("latitude is : " + response[0].lat + " and longitude " + response[0].lon);
                         // console.log(response[0].name);
 
-
+                        console.log("this is running two timesssssss ");
                         $.ajax({
                             method: "GET",
                             url: apiLocation,
@@ -130,12 +131,14 @@ $(document).ready(function () {
 
 
         }); //end of fetch data 
+
+
      
-    }//end of if
+    // }//end of if
 
 
     }); //end of on click event
 
 
 
-}); //end of ready Function
+ }); //end of ready Function
